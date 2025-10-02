7acbcf47886688eee4de6c9cb37b73c8b8a38911
hevolife <hevolife@gmail.com>
2025-10-02 15:17:20 +0200

    fix page booking

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import { PaymentPage } from './components/PaymentPage/PaymentPage';
 import { PaymentSuccessPage } from './components/PaymentPage/PaymentSuccessPage';
 import { PaymentCancelPage } from './components/PaymentPage/PaymentCancelPage';
+import { IframeBookingPage } from './components/IframeBooking/IframeBookingPage';
 import { LoginPage } from './components/Auth/LoginPage';
 import { ProtectedRoute } from './components/Auth/ProtectedRoute';
 import { AuthProvider } from './contexts/AuthContext';
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <Route path="/payment" element={<PaymentPage />} />
           <Route path="/payment-success" element={<PaymentSuccessPage />} />
           <Route path="/payment-cancel" element={<PaymentCancelPage />} />
+          <Route path="/booking/:userId" element={<IframeBookingPage />} />
           
           {/* Routes protégées - AVEC authentification */}
           <Route
